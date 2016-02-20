1db2cc5d97b709a71206c4f6a19bc039764cb92c
fabiomartinezgonzalez <fabiomartinezgonzalez@gmail.com>
2016-02-20 14:13:46 +0100

    Change from HTTP form to JSON in /user and /changePassword

--- voter-access/src/main/java/es/uniovi/asw/voteraccess/web/RESTVoterController.java
+++ voter-access/src/main/java/es/uniovi/asw/voteraccess/web/RESTVoterController.java
@@ -1,13 +1,12 @@
 package es.uniovi.asw.voteraccess.web;
 
 import es.uniovi.asw.voteraccess.model.Voter;
+import es.uniovi.asw.voteraccess.model.VoterRequestChangePassword;
+import es.uniovi.asw.voteraccess.model.VoterRequestGet;
 import es.uniovi.asw.voteraccess.service.voter.VoterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/rest")
 @RestController
@@ -17,23 +16,20 @@ public class RESTVoterController {
     private VoterService voterService;
 
     @RequestMapping(value = "/user",
-                    method = RequestMethod.POST,
-                    headers = "Accept=application/json",
-                    produces = "application/json")
+            method = RequestMethod.POST,
+            headers = "Accept=application/json",
+            produces = "application/json")
     @Transactional(readOnly = true)
-    public Voter getVoterInfo(@RequestParam("login") String email,
-                      @RequestParam("password") String password) {
-        return this.voterService.findByEmailAndPassword(email, password);
+    public Voter getVoterInfo(@RequestBody VoterRequestGet voterRequestGet) {
+        return this.voterService.findByEmailAndPassword(voterRequestGet.getLogin(), voterRequestGet.getPassword());
     }
 
     @RequestMapping(value = "/changepassword",
-                    method = RequestMethod.POST,
-                    headers = "Accept=application/json",
-                    produces = "application/json")
+            method = RequestMethod.POST,
+            headers = "Accept=application/json",
+            produces = "application/json")
     @Transactional(readOnly = true)
-    public void changePassword(@RequestParam("login") String email,
-                                  @RequestParam("oldPassword") String oldPassword,
-                                  @RequestParam("newPassword") String newPassword){
-        this.voterService.changePassword(email,oldPassword,newPassword);
+    public void changePassword(@RequestBody VoterRequestChangePassword voterRequestChangePassword) {
+        this.voterService.changePassword(voterRequestChangePassword.getLogin(), voterRequestChangePassword.getOldPassword(), voterRequestChangePassword.getNewPassword());
     }
 }
