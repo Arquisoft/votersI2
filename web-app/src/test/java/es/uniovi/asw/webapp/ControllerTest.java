bcc5d86867bca1944663b44e7f8efc37693af469
Luis Emilio Velasco Sanchez <emibloque@gmail.com>
2016-02-23 11:39:11 +0100

    Added support for web app testing

--- web-app/src/test/java/es/uniovi/asw/webapp/ControllerTest.java
+++ web-app/src/test/java/es/uniovi/asw/webapp/ControllerTest.java
@@ -27,7 +27,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
 @IntegrationTest("server.port:0")
-@Ignore
 public class ControllerTest {
 
     @Value("${voters.api}")
